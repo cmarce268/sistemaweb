3f7f27f3b9b6bcee9239bb772b8f0c63e2d49955
cmarce268 <clmarce268@gmail.com>
2023-03-09 12:05:28 -0500

    Actualizando archivos

--- modelos/vistamodel.php
+++ modelos/vistamodel.php
@@ -4,7 +4,7 @@
 		/*---Modelo para obtener las vsitas----*/
 		protected static function obtener_vistas_modelo($vistas){
 
-			$listaBlanca=["home","client-list","user-list","client-new","client-search","client-update","company","item-list","item-new","item-search","item-update","reservation-list"];
+			$listaBlanca=["home","client-list","user-list","client-new","client-search","client-update","company","item-list","item-new","item-search","item-update","reservation-list","reservation-new","reservation-pending","reservation-reservation"];
 			if (in_array($vistas, $listaBlanca)){
 				if(is_file("./vistas/contenidos/".$vistas."-view.php")){
 					$contenido="./vistas/contenidos/".$vistas."-view.php";
